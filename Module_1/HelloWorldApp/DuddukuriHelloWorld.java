0273ff4543c19a55788456b7fd661b3fa76cbdcf
venkatduddukuri <128104638+venkatduddukuri@users.noreply.github.com>
2023-03-17 07:07:09 +0530

    Assignment 1.2 GitHub Setup

---
+++ Module_1/HelloWorldApp/DuddukuriHelloWorld.java
@@ -0,0 +1,12 @@
+ @author Venkat
+ *
+ */
+public class DuddukuriHelloWorldApp {
+
+	static String firstName = "Venkatarao";
+	static String lastName= "Duddukuri";
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World from "+ firstName +" "+lastName);
+	}
+}
